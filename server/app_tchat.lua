6391b1f54ba440cdf428df38bb7f8af77cd0bd18
JericoFX <56100605+JericoFX@users.noreply.github.com>
2021-01-05 01:51:42 -0300

    Fix Dark Chat

--- server/app_tchat.lua
+++ server/app_tchat.lua
@@ -16,10 +16,28 @@ function TchatGetMessageChannel(a,b)
 end
 
 function TchatAddMessage(a,b)
+    --print(tostring(b))
     exports['ghmattimysql']:execute("INSERT INTO phone_app_chat (channel, message) VALUES(@channel, @message)",{['@channel']=a,['@message']=b},function(c)
-        exports['ghmattimysql']:execute("SELECT * from phone_app_chat WHERE id = @id",{['@id']=c},function(d)TriggerClientEvent('gcPhone:tchat_receive',-1,d[1])
+      ---  print(c.insertId)
+        exports['ghmattimysql']:execute("SELECT * from phone_app_chat WHERE id = @id",{['@id']=c.insertId},function(d)
+         --   print(tostring(d))
+            TriggerClientEvent('gcPhone:tchat_receive',-1,d[1])
         end)
     end)
+end
+function tprint (tbl, indent)
+    if not indent then indent = 0 end
+    for k, v in pairs(tbl) do
+        formatting = string.rep("  ", indent) .. k .. ": "
+        if type(v) == "table" then
+            print(formatting)
+            tprint(v, indent+1)
+        elseif type(v) == 'boolean' then
+            print(formatting .. tostring(v))
+        else
+            print(formatting .. v)
+        end
+    end
 end
 
 RegisterServerEvent('gcPhone:tchat_channel')
